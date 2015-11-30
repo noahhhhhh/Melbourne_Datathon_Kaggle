ad1b19cfbc7d136a3ccb6d5a01f516bcdad29278
noahhhhhh <noah.xm.shaw@gmail.com>
2015-11-30 20:42:46 +1100

    minor changes

--- 6_cv.R
+++ 6_cv.R
@@ -7,237 +7,61 @@ require(data.table)
 require(dplyr)
 require(caret)
 require(caTools)
-load("../Datathon_Full_Dataset/transformedData.RData")
+load("../Datathon_Full_Dataset/trainData.RData")
+load("../Datathon_Full_Dataset/valid1Data.RData")
+load("../Datathon_Full_Dataset/valid2Data.RData")
+load("../Datathon_Full_Dataset/testData.RData")
 # dtTestFeatures <- fread("../data_files/semi_and_final_features.csv")
 dtSampleSubmit <- fread("../data_files/sample_submission_bet_size.csv")
 dtSampleSubmit[, Prediction := NULL]
 setnames(dtSampleSubmit, "Account_ID")
-#######################################
-## 1.0 train, valid, and test set #####
-#######################################
+#####################################################################
+## 1. cv the xgboost ################################################
+#####################################################################
 ##############################
-## 1.1 train set #############
+## 1.1 train, valid, and test transform
 ##############################
-dt.train <- dt.scaledAll[!UNIT %in% c("36_37_38", "37_38_39", "38_39_40" # valid set 1 related
-                                      , "39_40_41", "40_41_42", "41_42_43" # valide set 2 related
-                                      , "42_43_44", "43_44_45", "44_45_46") # test set related
-                         , with = T]
-dt.train[, UNIT := NULL]
-# dt.train[, ACCOUNT_ID := NULL]
-dt.train[, THIS_PROFIT_LOSS := NULL]
-str(dt.train)
-dim(dt.train)
-# [1] 256719     59
+require(xgboost)
+require(Ckmeans.1d.dp)
+x.train <- model.matrix(PRED ~., dt.train[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.train <- ifelse(as.integer(dt.train$PRED) == 1, 0, 1)
+dmx.train <- xgb.DMatrix(data =  x.train, label = y.train)
 
-##############################
-## 1.2 valid set #############
-##############################
-# valid set 1
-dt.valid1 <- dt.scaledAll[UNIT == c("38_39_40"), with = T]
-dt.valid1[, UNIT := NULL]
-# dt.valid[, ACCOUNT_ID := NULL]
-dt.valid1[, THIS_PROFIT_LOSS := NULL]
-str(dt.valid1)
-dim(dt.valid1)
-# [1] 6791   59
+x.valid1 <- model.matrix(PRED ~., dt.valid1[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.valid1 <- ifelse(as.integer(dt.valid1$PRED) == 1, 0, 1)
+dmx.valid1 <- xgb.DMatrix(data =  x.valid1, label = y.valid1)
 
-# valid set 2
-dt.valid2 <- dt.scaledAll[UNIT == c("41_42_43"), with = T]
-dt.valid2[, UNIT := NULL]
-# dt.valid[, ACCOUNT_ID := NULL]
-dt.valid2[, THIS_PROFIT_LOSS := NULL]
-str(dt.valid2)
-dim(dt.valid2)
-# [1] 8082   59
+x.valid2 <- model.matrix(PRED ~., dt.valid2[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.valid2 <- ifelse(as.integer(dt.valid2$PRED) == 1, 0, 1)
+dmx.valid2 <- xgb.DMatrix(data =  x.valid2, label = y.valid2)
+
+x.test <- model.matrix(~., dt.test[, !c("ACCOUNT_ID"), with = F])[, -1]
 
 ##############################
-## 1.3 test set #############
+## 1.2 cv ####################
 ##############################
-dt.test <- dt.scaledAll[UNIT == c("44_45_46"), with = T]
-dt.test[, UNIT := NULL]
-# dt.test[, ACCOUNT_ID := NULL]
-dt.test[, THIS_PROFIT_LOSS := NULL]
-dt.test[, PRED := NULL]
-str(dt.test)
-dim(dt.test)
-# [1] 12935    58
-
-#######################################
-## 2.0 try to train a model  ##########
-#######################################
-##############################
-## 2.1 bagged tree ###########
-##############################
-require(randomForest)
-set.seed(1)
-md.bagTree <- randomForest(PRED ~., data = dt.train[, !c("ACCOUNT_ID"), with = F], mtry = 57, ntree = 20, importance = T)
-md.bagTree
-# Call:
-#     randomForest(formula = PRED ~ ., data = dt.train[, !c("ACCOUNT_ID"),      with = F], mtry = 54, ntree = 20, importance = T) 
-# Type of random forest: classification
-# Number of trees: 20
-# No. of variables tried at each split: 54
-# 
-# OOB estimate of  error rate: 12.92%
-# Confusion matrix:
-#     0      1 class.error
-# 0 135726  20682   0.1322311
-# 1  19267 133461   0.1261524
-
-varImpPlot(md.bagTree)
-
-# try on valid set
-pred.valid <- predict(md.bagTree, newdata = dt.valid[, !c("ACCOUNT_ID"), with = F])
-pred.valid <- as.numeric(ifelse(pred.valid == 1, 1, 0))
-colAUC(pred.valid, dt.valid$PRED, plotROC = T)
-# [,1]
-# 0 vs. 1 0.9248425
-table(pred.valid, dt.valid$PRED)
-# Confusion Matrix and Statistics
-# 
-# Reference
-# Prediction    0    1
-# 0 9214  229
-# 1  457 1993
-# 
-# Accuracy : 0.9423         
-# 95% CI : (0.938, 0.9464)
-# No Information Rate : 0.8132         
-# P-Value [Acc > NIR] : < 2.2e-16      
-# 
-# Kappa : 0.8174         
-# Mcnemar's Test P-Value : < 2.2e-16      
-# 
-# Sensitivity : 0.9527         
-# Specificity : 0.8969         
-# Pos Pred Value : 0.9757         
-# Neg Pred Value : 0.8135         
-# Prevalence : 0.8132         
-# Detection Rate : 0.7747         
-# Detection Prevalence : 0.7940         
-# Balanced Accuracy : 0.9248         
-# 
-# 'Positive' Class : 0 
-
-# try on test set
-pred.test <- predict(md.bagTree, newdata = dt.test[, !c("ACCOUNT_ID"), with = F])
-table(pred.test)
-# pred.test
-# 0     1 
-# 11725  1210 
-dt.submit <- data.table(Account_ID = dt.test$ACCOUNT_ID, Prediction = pred.test)
-dt.submit <- merge(dtSampleSubmit, dt.submit, by = "Account_ID", all.x = T, all.y = F)
-write.csv(dt.submit, "submit/1_191115_1727_bagTree_with_3in1.csv", row.names = FALSE)
-
-##############################
-## 2.2 random forest #########
-##############################
-require(randomForest)
-set.seed(1)
-md.rf <- randomForest(PRED ~.
-                      , data = dt.scaledAll[, !c("ACCOUNT_ID", "UNIT", "THIS_PROFIT_LOSS"), with = F]
-                      , mtry = floor(sqrt(57))
-                      , ntree = 100
-                      , importance = T)
-md.rf
-# Call:
-#     randomForest(formula = PRED ~ ., data = dt.train[, !c("ACCOUNT_ID"),      with = F], mtry = floor(sqrt(57)), ntree = 20, importance = T) 
-# Type of random forest: classification
-# Number of trees: 20
-# No. of variables tried at each split: 7
-# 
-# OOB estimate of  error rate: 16.81%
-# Confusion matrix:
-#     0      1 class.error
-# 0 110180  24172   0.1799154
-# 1  21820 117483   0.1566370
-
-varImpPlot(md.rf)
-# try on valid set
-pred.valid1 <- predict(md.rf, newdata = dt.valid1[, !c("ACCOUNT_ID"), with = F])
-pred.valid1 <- as.numeric(ifelse(pred.valid1 == 1, 1, 0))
-colAUC(pred.valid1, dt.valid1$PRED, plotROC = T)
-# [,1]
-# 0 vs. 1 0.815286
-table(pred.valid1, dt.valid1$PRED)
-# pred.valid    0    1
-# 1 3492  691
-# 2  803 3096
-confusionMatrix(pred.valid1, dt.valid1$PRED)
-# Confusion Matrix and Statistics
-# 
-# Reference
-# Prediction    0    1
-# 0 3496  675
-# 1  799 3112
-# 
-# Accuracy : 0.8176        
-# 95% CI : (0.809, 0.826)
-# No Information Rate : 0.5314        
-# P-Value [Acc > NIR] : < 2.2e-16     
-# 
-# Kappa : 0.6345        
-# Mcnemar's Test P-Value : 0.001357      
-#                                         
-#             Sensitivity : 0.8140        
-#             Specificity : 0.8218        
-#          Pos Pred Value : 0.8382        
-#          Neg Pred Value : 0.7957        
-#              Prevalence : 0.5314        
-#          Detection Rate : 0.4326        
-#    Detection Prevalence : 0.5161        
-#       Balanced Accuracy : 0.8179        
-#                                         
-#        'Positive' Class : 0 
-
-pred.valid2 <- predict(md.rf, newdata = dt.valid2[, !c("ACCOUNT_ID"), with = F])
-pred.valid2 <- as.numeric(ifelse(pred.valid2 == 1, 1, 0))
-colAUC(pred.valid2, dt.valid2$PRED, plotROC = T)
-# [,1]
-# 0 vs. 1 0.815286
-table(pred.valid2, dt.valid2$PRED)
-# pred.valid    0    1
-# 1 3492  691
-# 2  803 3096
-confusionMatrix(pred.valid2, dt.valid2$PRED)
+params <- list(nthread = 8
+               , objective = "binary:logistic"
+               , eval_metric = "auc"
+               , eta = .025
+               , subsample = .8
+               , colsample_bytree = .3)
+nrounds <- 
+cv.xgboost <- xgb.cv(params = params
+                     , data = dmx.train
+                     , nrounds = nrounds
+                     , nfold = 5
+                     , showsd = T
+                     , verbose = T)
 
 
-pred.test <- predict(md.rf, newdata = dt.test[, !c("ACCOUNT_ID"), with = F])
-table(pred.test)
-# pred.test
-# 0    1 
-# 9801 3134 
-dt.submit <- data.table(Account_ID = dt.test$ACCOUNT_ID, Prediction = pred.test)
-dt.submit <- merge(dtSampleSubmit, dt.submit, by = "Account_ID", all.x = T, all.y = F)
-write.csv(dt.submit, "submit/5_201115_2117_rf_with_3in1_corrected_train_and_valid.csv", row.names = FALSE) # 0.53..
 
-##############################
-## 2.3 boosting tree #########
-##############################
-require(gbm)
-set.seed(1)
-md.boostTree <- gbm(PRED ~., data = dt.train[, !c("ACCOUNT_ID"), with = F]
-                    , distribution = "adaboost"
-                    , n.trees = 200
-                    , shrinkage = 1
-                    )
-md.boostTree <- gbm.fit(x = dt.train[, !c("ACCOUNT_ID", "PRED"), with = F]
-                        , y = dt.train$PRED)
-md.boostTree
-summary(md.boostTree)
 
-# try on valid set
-pred.valid <- predict(md.boostTree, newdata = dt.valid[, !c("ACCOUNT_ID"), with = F], n.trees = 200)
-pred.valid <- as.numeric(ifelse(pred.valid == 1, 1, 0))
-colAUC(pred.valid, dt.valid$PRED, plotROC = T)
 
-table(pred.valid, dt.valid$PRED)
 
-confusionMatrix(pred.valid, dt.valid$PRED)
 
-# try on test
-pred.test <- predict(md.boostTree, newdata = dt.test[, !c("ACCOUNT_ID"), with = F])
-table(pred.test)
+
+
 
 
 
--- 7_model.R
+++ 7_model.R
@@ -254,6 +254,8 @@ md.xgboost7 <- xgb.train(data = dmx.train
                          , nrounds = 1700
                          , verbose = T)
 
+logit2prop <- function(l){exp(l)/(1+exp(l))}
+
 # valid1
 pred.valid1.1 <- predict(md.xgboost1, x.valid1)
 pred.valid1.2 <- predict(md.xgboost2, x.valid1)
@@ -301,8 +303,8 @@ importance_matrix1 <- xgb.importance(colnames(x.train), model = md.xgboost1)
 print(importance_matrix1)
 xgb.plot.importance(importance_matrix1)
 
-pred.valid1 <- pred.valid1.1 + pred.valid1.2 + pred.valid1.3 + pred.valid1.4
-                   + pred.valid1.5 + pred.valid1.6 + pred.valid1.7
+pred.valid1 <- logit2prop(pred.valid1.1) + logit2prop(pred.valid1.2) + logit2prop(pred.valid1.3) + logit2prop(pred.valid1.4)
+                   + logit2prop(pred.valid1.5) + logit2prop(pred.valid1.6) + logit2prop(pred.valid1.7)
 colAUC(pred.valid1, y.valid1)
 # [,1]
 # 0 vs. 1 0.8520213
@@ -354,8 +356,8 @@ colAUC(pred.valid2.7, y.valid2)
 # [,1]
 # 0 vs. 1 0.7356207
 
-pred.valid2 <- pred.valid2.1 + pred.valid2.2 + pred.valid2.3 + pred.valid2.4
-                   + pred.valid2.5 + pred.valid2.6 + pred.valid2.7
+pred.valid2 <- logit2prop(pred.valid2.1) + logit2prop(pred.valid2.2) + logit2prop(pred.valid2.3) + logit2prop(pred.valid2.4)
+                   + logit2prop(pred.valid2.5) + logit2prop(pred.valid2.6) + logit2prop(pred.valid2.7)
 colAUC(pred.valid2, y.valid2)
 # [,1]
 # 0 vs. 1 0.7341297
@@ -364,24 +366,24 @@ colAUC(pred.valid2, y.valid2)
 ##############################
 ## 3.3 submit
 ##############################
-pred.test1 <- predict(md.xgboost1, x.test)
-pred.test2 <- predict(md.xgboost2, x.test)
-pred.test3 <- predict(md.xgboost3, x.test)
-pred.test4 <- predict(md.xgboost4, x.test)
-pred.test5 <- predict(md.xgboost5, x.test)
-pred.test6 <- predict(md.xgboost6, x.test)
-pred.test7 <- predict(md.xgboost7, x.test)
+pred.test1 <- logit2prop(predict(md.xgboost1, x.test))
+pred.test2 <- logit2prop(predict(md.xgboost2, x.test))
+pred.test3 <- logit2prop(predict(md.xgboost3, x.test))
+pred.test4 <- logit2prop(predict(md.xgboost4, x.test))
+pred.test5 <- logit2prop(predict(md.xgboost5, x.test))
+pred.test6 <- logit2prop(predict(md.xgboost6, x.test))
+pred.test7 <- logit2prop(predict(md.xgboost7, x.test))
 
 pred.test <- pred.test1 + pred.test2 + pred.test3 + pred.test4
                  + pred.test5 + pred.test6 + pred.test7
-
+pred.test.tree <- pred.test
 table(pred.test)
 dt.submit <- data.table(Account_ID = dt.test$ACCOUNT_ID, Prediction = pred.test)
 dim(dt.submit)
 # [1] 12935     2
 dt.submit <- merge(dtSampleSubmit, dt.submit, by = "Account_ID", all.x = T, sort = F)
 # [1] 7374    2
-write.csv(dt.submit, "submit/10_291115_2231_7_xgboost_with_random_3in1_preprocess_valid1_valid2_.csv", row.names = F) # 0.62915
+write.csv(dt.submit, "submit/12_301115_1837_7_xgboost_combined_prob_with_random_3in1_preprocess_valid1_valid2_.csv", row.names = F) # 0.62915
 
 #####################################################################
 ## 4. try 3in1 non-overlap data on automated xgboost ################
@@ -453,7 +455,148 @@ dt.submit <- merge(dtSampleSubmit, dt.submit, by = "Account_ID", all.x = T, sort
 # [1] 7374    2
 write.csv(dt.submit, "submit/9_291115_0944_50_xgboost_with_non_overlap_3in1_preprocess_valid1_valid2_.csv", row.names = F) # 0.61628
 
+#####################################################################
+## 5. try 3in1 data on xgboost - regression #########################
+#####################################################################
+##############################
+## 5.1 train, valid, and test transform
+##############################
+require(xgboost)
+require(Ckmeans.1d.dp)
+x.train <- model.matrix(PRED ~., dt.train[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.train <- ifelse(as.integer(dt.train$PRED) == 1, 0, 1)
+dmx.train <- xgb.DMatrix(data =  x.train, label = y.train)
 
+x.valid1 <- model.matrix(PRED ~., dt.valid1[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.valid1 <- ifelse(as.integer(dt.valid1$PRED) == 1, 0, 1)
+dmx.valid1 <- xgb.DMatrix(data =  x.valid1, label = y.valid1)
 
+x.valid2 <- model.matrix(PRED ~., dt.valid2[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.valid2 <- ifelse(as.integer(dt.valid2$PRED) == 1, 0, 1)
+dmx.valid2 <- xgb.DMatrix(data =  x.valid2, label = y.valid2)
 
+x.test <- model.matrix(~., dt.test[, !c("ACCOUNT_ID"), with = F])[, -1]
+
+##############################
+## 5.2 model - xgboost - gblinear
+##############################
+set.seed(1)
+md.xgboost1.gblinear <- xgb.train(data = dmx.train
+                         , booster = "gblinear"
+                         , params = list(nthread = 8
+                                         , eval_metric = "auc"
+                                         , eta = .025
+                                         , max_depth = 6
+                                         , subsample = .8
+                                         , colsample_bytree = .3)
+                         , watchlist = list(eval1 = dmx.valid1
+                                            , eval2 = dmx.valid2
+                                            , train = dmx.train)
+                         , nrounds = 1700
+                         , verbose = T)
+
+# valid1
+pred.valid1.1 <- predict(md.xgboost1.gblinear, x.valid1)
+
+colAUC(pred.valid1.1, y.valid1)
+# [,1]
+# 0 vs. 1 0.7264964
+importance_matrix1 <- xgb.importance(colnames(x.train), model = md.xgboost1)
+print(importance_matrix1)
+xgb.plot.importance(importance_matrix1)
+
+# valid2
+pred.valid2.1 <- predict(md.xgboost1.gblinear, x.valid2)
+
+colAUC(pred.valid2.1, y.valid2)
+# [,1]
+# 0 vs. 1 0.6307768
+
+pred.test <- predict(md.xgboost1.gblinear, x.test)
+pred.test.linear <- pred.test * 7
+pred.test.linear
+pred.test.tree
+pred.test <- pred.test.linear + pred.test.tree
+##############################
+## 5.3 submit
+##############################
+dt.submit <- data.table(Account_ID = dt.test$ACCOUNT_ID, Prediction = pred.test)
+dim(dt.submit)
+# [1] 12935     2
+dt.submit <- merge(dtSampleSubmit, dt.submit, by = "Account_ID", all.x = T, sort = F)
+# [1] 7374    2
+write.csv(dt.submit, "submit/13_301115_1842_7_tree_xgboost_and_1_linear_xgboost_with_combined_3in1_preprocess_valid1_valid2_.csv", row.names = F) # 0.61693
+
+#####################################################################
+## 6. try 3in1 random data on automated xgboost ################
+#####################################################################
+##############################
+## 6.1 train, valid, and test transform
+##############################
+require(xgboost)
+require(Ckmeans.1d.dp)
+x.train <- model.matrix(PRED ~., dt.train[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.train <- ifelse(as.integer(dt.train$PRED) == 1, 0, 1)
+dmx.train <- xgb.DMatrix(data =  x.train, label = y.train)
+
+x.valid1 <- model.matrix(PRED ~., dt.valid1[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.valid1 <- ifelse(as.integer(dt.valid1$PRED) == 1, 0, 1)
+dmx.valid1 <- xgb.DMatrix(data =  x.valid1, label = y.valid1)
+
+x.valid2 <- model.matrix(PRED ~., dt.valid2[, !c("ACCOUNT_ID"), with = F])[, -1]
+y.valid2 <- ifelse(as.integer(dt.valid2$PRED) == 1, 0, 1)
+dmx.valid2 <- xgb.DMatrix(data =  x.valid2, label = y.valid2)
+
+x.test <- model.matrix(~., dt.test[, !c("ACCOUNT_ID"), with = F])[, -1]
+
+##############################
+## 6.2 model - xgboost
+#############################
+eta <- rep(.025, 7)
+# max_depth <- c(8, 7, 7, 6, 5)
+# min.child <- c(5, 5, 5, 5, 5)
+subsample <- rep(.8, 7)
+colsample_bytree <- rep(.3, 7)
+nrounds <- rep(1700, 7)
+
+reps <- 1
+models <- 7
+
+pred.test <- rep(0, nrow(dtSampleSubmit))
+
+for (i in 1:reps){
+    print(paste("rep", i, "- Start"))
+    for(j in 1:models){
+        print(paste("------model", j, ": Start"))
+        # set.seed(1000 * i + 100 * j)
+        set.seed(j)
+        md.xgboost <- xgb.train(data = dmx.train
+                                , objective = "binary:logistic"
+                                , params = list(nthread = 8
+                                                , eval_metric = "auc"
+                                                , eta = eta[j]
+                                                # , max_depth = max_depth[j]
+                                                , subsample = subsample[j]
+                                                , colsample_bytree = colsample_bytree[j])
+                                , watchlist = list(eval1 = dmx.valid1
+                                                   , eval2 = dmx.valid2
+                                                   , train = dmx.train)
+                                , nrounds = nrounds[j]
+                                , verbose = T)
+        pred.test <- pred.test + predict(md.xgboost, x.test)
+        print(paste("------model", j, ": End"))
+    }
+    print(paste("rep", i, "- End"))
+}
+pred.test
+
+##############################
+## 6.3 submit
+##############################
+dt.submit <- data.table(Account_ID = dt.test$ACCOUNT_ID, Prediction = pred.test)
+dim(dt.submit)
+# [1] 12935     2
+dt.submit <- merge(dtSampleSubmit, dt.submit, by = "Account_ID", all.x = T, sort = F)
+# [1] 7374    2
+write.csv(dt.submit, "submit/9_291115_0944_50_xgboost_with_non_overlap_3in1_preprocess_valid1_valid2_.csv", row.names = F) # 0.61628
 
